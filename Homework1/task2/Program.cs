a29bf676a7079ba81f576ae6c4de04ce29e26f62
AnnaLuka07 <aanyta07@gmail.com>
2022-10-19 20:13:26 +0300

    task1234

---
+++ Homework1/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Например: 2,3,7 > 7
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+Console.WriteLine($"Максимальное из этих чисел - {max}");
---
+++ Homework1/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (n < count )
+{
+    Console.WriteLine("Введите число больше 1");
+}
+while ( count <= n & count % 2 == 0 )
+{
+   Console.WriteLine(count);
+   count = count + 2; 
+}
